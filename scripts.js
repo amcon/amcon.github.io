75a404a431d1fb94f2036bfffe396bcb89b57f05
Aaron Conklin <conklin.aaron@gmail.com>
2016-12-03 19:12:50 -0500

    Consistency and JS functions

--- scripts.js
+++ scripts.js
@@ -1,1 +1,36 @@
 console.log("I'm here");
+
+$(function(){
+
+  function go_to_pokemon(){
+    window.location.href = "https://amcon.github.io/GA_Project_1/";
+  }
+
+  function go_to_songsterr(){
+    window.location.href = "http://songsterr-tab-db.herokuapp.com/";
+  }
+
+  function go_to_chairShare(){
+    window.location.href = "http://chairshare.herokuapp.com/";
+  }
+
+  function go_to_resume(){
+    window.location.href = "https://docs.google.com/document/d/11CP65LGIsMTWFA8F3Y_tHfGzwvZGNUJRFOR3RaNVD_g/edit?usp=sharing";
+  }
+
+  $(".project-one").click(function() {
+  go_to_pokemon();
+  });
+
+  $(".project-two").click(function() {
+    go_to_songsterr();
+  });
+
+  $(".project-three").click(function(){
+    go_to_chairShare();
+  });
+
+  $(".resume").click(function(){
+    go_to_resume();
+  })
+})
